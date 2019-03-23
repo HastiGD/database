87dd855c250b2f85e8557dc514e3f97b1f21b308
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2019-03-22 21:10:58 -0700

    added some last minute functions

--- spaceship.py
+++ spaceship.py
@@ -13,25 +13,10 @@ for i, line in enumerate(file):
 https://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
 '''
 
-#import turtle
-#turtle.hideturtle()
+import turtle
+turtle.hideturtle()
 
 import random
-
-def main():
-    ship_body = input("Do you want to draw the body? Y/N\n")
-    if ship_body == "Y":
-        draw_ship_body()
-    else:
-        print("You chose No")
-        
-    left_rocket = input("Do you want to draw the left rocket? Y/N\n")
-    if left_rocket == "Y":
-        draw_left_rocket()
-    else:
-        print("You chose No")
-
-   
 
 def draw_ship_body():
     turtle.penup()
@@ -80,9 +65,8 @@ def draw_right_flame():
     turtle.goto(60,-130)
     turtle.goto(50,-150)
     turtle.goto(40,-130)
-
+    
 def draw_blanks(num_of_letters):
-    num_of_letters += 1
     for i in range(num_of_letters):
         distance_between_blanks = int((20*i) + 10)
         length_of_blank = int((20*i))
@@ -91,36 +75,77 @@ def draw_blanks(num_of_letters):
         turtle.pendown()
         turtle.goto(distance_between_blanks,-200)
 
-def write_letter(location, letter):
+def write_letter(xlocation, ylocation, letter):
     turtle.penup()
-    turtle.goto(location,-197)
+    turtle.goto(xlocation,ylocation)
     turtle.pendown()
     turtle.write(letter, align="center", font=("Arial", 12, "normal"))
 
-def count_words_in_list(filename):
+def create_list_of_words_from_file(filename):
     file = open(filename, "r")
-    for i, line in enumerate(file):
-        pass
-    num_words = i
-    file.close()
-    return num_words
-    #print(num_words)
+    wordList = []
+    for words in file:
+        current_word = words.strip()
+        wordList.append(current_word)
+    file.close
+    return wordList
 
-def choose_word(filename):
-    file = open(filename, "r")
-    count_words_in_list(filename)
+def choose_random_word_from_list(a_list):
+    rand_word = random.choice(a_list)
+    return rand_word
 
-    word_index = random.randint(0
-    rand_word_index = random.randint(0, num_words)
-    print(rand_word_index)
-    word = file.readlines()
-    print(word[0])
-    file.close()
-    #return print(rand_word)
+def prompt_user_for_guess():
+    keep_prompting = True
+    
+    while keep_prompting:
+        user_guess = input("Guess a letter\n")
+        user_guess = user_guess.lower()
+        if len(user_guess) > 1:
+            print("Please guess one letter at a time")
+        if user_guess.isalpha() == False:
+            print("Please enter a letter")
+        if len(user_guess) == 1 and user_guess.isalpha():
+            keep_prompting = False
+            return user_guess
 
-choose_word("wordlist.txt")
+def create_list_from_secret_word(word):
+    list_of_characters_in_secret_word = []
+    for character in word:
+        list_of_characters_in_secret_word.append(character)
+    return list_of_characters_in_secret_word
 
+def check_if_secret_word_contains_user_guess(list_from_secret_word, user_guess):
+    indices = [i for i, character in enumerate(list_from_secret_word) if character == user_guess]
+    return indices
+
+def draw_next_part_of_ship(number, letter):
+    if number == 0:
+        draw_ship_body()
+        write_letter((75 - 10*number), 200, letter)
+        
+    if number == 1:
+        draw_left_rocket()
+        write_letter((75 - 10*number), 200, letter)
+        
+    if number == 2:
+        draw_right_rocket()
+        write_letter((75 - 10*number), 200, letter)
+        
+    if number == 3:
+        draw_left_flame()
+        write_letter((75 - 10*number), 200, letter)
+    
+    if number == 4:
+        draw_right_flame()
+        write_letter((75 - 10*number), 200, letter)
+
+def write_score_to_file(scoresfile, player_name, score):
+    scoresfile = open(scoresfile, "a")
+    scoresfile.write("\n {} scored {} points".format(player_name, score))
+    scoresfile.close()
+    
 """
+
 main()
 draw_ship_body()
 draw_left_rocket()
